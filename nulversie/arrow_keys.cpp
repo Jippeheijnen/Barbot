a12a8d24e67c4c108a5c6a51d081a61d35717238
DaanWesterhof <43374529+DaanWesterhof@users.noreply.github.com>
2019-02-27 17:12:45 +0100

    andere module gebruit, werkt nu met windows

--- nulversie/arrow_keys.cpp
+++ nulversie/arrow_keys.cpp
@@ -1,6 +1,10 @@
+#include <conio.h>
 #include <iostream>
-#include <winuser.h>
-#include <signal.h>
+using namespace std;
+#define KEY_UP 72
+#define KEY_DOWN 80
+#define KEY_LEFT 75
+#define KEY_RIGHT 77
 
 
 int main() {
@@ -9,65 +13,83 @@ int main() {
     bool left;
     bool right;
     bool back;
-    while (running){
-        if (GetAsyncKeyState(0x26)){
-            if(straight){
-                continue;
-            }else{
-                left = false;
-                straight = true;
-                right = false;
-                back = false;
-                std::cout << "straight";
+    int c = 0;
+    while (running) {
+        c = _getch();
+        switch ((c = _getch())) {
+            case KEY_UP:
+                if(straight){
+                    continue;
+                }else{
+                    left = false;
+                    straight = true;
+                    right = false;
+                    back = false;
+                    std::cout << "straight";
 
-                //code straight
-            }
+                    //code straight
+                }
+
+                break;
+
+            case KEY_DOWN:
+                if(back){
+                    continue;
+                }else {
+                    left = false;
+                    straight = false;
+                    right = false;
+                    back = true;
+                    std::cout << "back";
+
+                    //code back
+                }
+                break;
+
+            case KEY_LEFT:
+                if(left){
+                    continue;
+                }else{
+                    left = true;
+                    straight = false;
+                    right = false;
+                    back = false;
+                    std::cout << "left";
+
+                    //code left
+                }
+                break;
+
+            case KEY_RIGHT:
+                if(right){
+                    continue;
+                }else{
+                    left = false;
+                    straight = false;
+                    right = true;
+                    back = false;
+                    std::cout << "right";
+
+                    //code right
+                }
+                break;
+
+            default:
+                if(straight){
+                    //code stop bij straight
+                    straight = false;
+                }else if(left){
+                    //code stop bij left
+                    left = false;
+                }else if(right){
+                    //code stop bij right
+                    right = false;
+                }else if(back){
+                    //code stop bij back
+                    back = false;
+
+                break;
         }
-
-        else if (GetAsyncKeyState(0x25)){
-
-            if(left){
-                continue;
-            }else{
-                left = true;
-                straight = false;
-                right = false;
-                back = false;
-                std::cout << "left";
-
-                //code left
-            }
-        }
-
-        else if (GetAsyncKeyState(0x27)){
-            if(right){
-                continue;
-            }else{
-                left = false;
-                straight = false;
-                right = true;
-                back = false;
-                std::cout << "right";
-
-                //code right
-            }
-        }
-
-        else if (GetAsyncKeyState(0x28)){
-            if(back){
-                continue;
-            }else {
-                left = false;
-                straight = false;
-                right = false;
-                back = true;
-                std::cout << "back";
-
-                //code back
-            }
-
-        }
-
     }
-
+}
 }
