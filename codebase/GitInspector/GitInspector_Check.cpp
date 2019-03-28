67de8df420261bc8ffb4db897b23ee73fde34d3f
Niels Post <niels.maff.pc@gmail.com>
2019-03-29 00:25:19 +0100

    - Commented GitInspector runner

--- codebase/GitInspector/GitInspector_Check.cpp
+++ codebase/GitInspector/GitInspector_Check.cpp
@@ -9,19 +9,27 @@ void gitinspector_validate();
 void python27_validate();
 void gitinspector_run();
 
+
 int main() {
     std::cout << std::endl << std::endl;
     python27_validate();
     gitinspector_validate();
     gitinspector_run();
-
+    return 0;
 }
+
+/**
+ * Runs gitinspector on the current repository
+ */
 void gitinspector_run() {
     std::cout << "Alright! running Gitinspector, hold tight, this might take some time" << std::endl;
     std::string command = PYTHON27_PATH + " gitinspector/gitinspector.py --format=htmlembedded --timeline -w .. > ../codebase/GitInspector/Gitinspector_stats.html";
     system(command.c_str());
 }
 
+/**
+ * Checks if gitinspector is found in the build folder, if not, clones it
+ */
 void gitinspector_validate() {
     std::cout << "Checking if gitinspector is already in the build folder" << std::endl;
     std::ifstream gitins("gitinspector/gitinspector.py");
@@ -36,6 +44,9 @@ void gitinspector_validate() {
     gitins.close();
 }
 
+/**
+ * Checks if Python 2.7 is installed in the default location, runs the setup if it's not
+ */
 void python27_validate() {
     std::cout << "Validating if Python2.7 is installed in the default location" << std::endl;
     std::ifstream python27(PYTHON27_PATH);
