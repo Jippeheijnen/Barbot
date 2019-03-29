ae8c4d788363e6e2475a0ce8741e779d00894f1e
Niels Post <niels.maff.pc@gmail.com>
2019-03-29 15:52:07 +0100

    - Header Files for Cup Detection

---
+++ codebase/BarBot/CupDetection.cpp
@@ -0,0 +1,13 @@
+//
+// Created by niels on 3/29/2019.
+//
+
+#include "CupDetection.h"
+
+void CupDetection::init() {
+
+}
+
+bool CupDetection::isCupPlaced() {
+    return false;
+}
---
+++ codebase/BarBot/CupDetection.h
@@ -0,0 +1,18 @@
+//
+// Created by niels on 3/29/2019.
+//
+
+#ifndef BARBOT_CUPDETECTION_H
+#define BARBOT_CUPDETECTION_H
+
+
+class CupDetection {
+private:
+
+public:
+    void init();
+    bool isCupPlaced();
+};
+
+
+#endif //BARBOT_CUPDETECTION_H
