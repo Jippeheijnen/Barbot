d84a1da113fbe3697ff745c6055f97372ff3ef64
mickb <mick.bos@student.hu.nl>
2019-04-01 14:45:46 +0200

    Made LineFollow pausable

--- codebase/BarBot/LineFollow.cpp
+++ codebase/BarBot/LineFollow.cpp
@@ -7,31 +7,31 @@
 LineDetection LD;
 Movement movement;
 
-void LineFollow::follow() {
-//    if(ColorReading.hasChanged == true){cout << ColorReading.color;}
-    LD.init(2000, 20);
-    movement.init();
-    double SensorInput = LD.getLineDirection();
-    std::cout << SensorInput << std::endl;
-    if (SensorInput < 0) {
-        movement.leftSpeed(((100+SensorInput)/100)*30);
-        movement.rightSpeed(30);
-    } else if (SensorInput > 0) {
-        movement.rightSpeed(((100-SensorInput)/100)*30);
-        movement.leftSpeed(30);
-    }
-    usleep(1000);
+bool toBePaused = false;
+
+void LineFollow::pause() {
+    movement.stop();
+    toBePaused = true;
 }
 
-//void LineFollow::destination(){
-//    if(ColorReading.hasChanged==true && ColorReading.color == DestinationColor){
-//        M.stop()
-//    }
-//}
+void LineFollow::resume() {
+    toBePaused = false;
+}
 
-//double SensorInput = LineDetection::getLineDirection();
-//if (SensorInput > 0) {
-//BP.set_motor_power(MOTOR_STEER,(SensorInput&3.65));
-//} else {
-//BP.set_motor_position_relative(MOTOR_STEER,(SensorInput*3.65));
-//}
+void LineFollow::follow() {
+//    if(ColorReading.hasChanged == true){cout << ColorReading.color;}
+    if(!toBePaused) {
+        LD.init(2000, 20);
+        movement.init();
+        double SensorInput = LD.getLineDirection();
+        std::cout << SensorInput << std::endl;
+        if (SensorInput < 0) {
+            movement.leftSpeed(((100 + SensorInput) / 100) * 30);
+            movement.rightSpeed(30);
+        } else if (SensorInput > 0) {
+            movement.rightSpeed(((100 - SensorInput) / 100) * 30);
+            movement.leftSpeed(30);
+        }
+        usleep(1000);
+    }
+}
