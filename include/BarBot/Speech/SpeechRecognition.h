47fe454a58b279d49ae3dad8405942f4c51ad60a
DaanW <daan.westerhof@student.hu.nl>
2019-04-09 20:20:47 +0200

    added doc strings

--- include/BarBot/Speech/SpeechRecognition.h
+++ include/BarBot/Speech/SpeechRecognition.h
@@ -25,7 +25,23 @@ private:
     //step variables
 public:
     static const std::string TAG, TAG_PYTHON;
+    /**
+     * Forks the code to a child. The child creates a pipe and
+     * initiates the python script that listens with a usb microphone,
+     * and connects with the google speech api.
+     * Result streamed to the pipe.
+     * @param linFol
+     * @param pumpSer
+     * @param cupDet
+     */
     void init(LineFollow *linFol, DrinkService *pumpSer, CupDetection *cupDet);
+
+    /**
+     * Reads out the pipe.
+     * The recieved char array gets split into words, words are pushed to a vector.
+     * Vector gets returned.
+     * @return
+     */
     std::vector<std::string> poll();
 
 };
